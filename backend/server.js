4e25afec75640964daf5638c03dfafd30400b7ba
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-05-23 00:07:19 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(express.json())
 
 if(process.env.NODE_ENV === 'development')
   app.use(morgan('dev'));
-
+ 
 app.use('/api/v1/transaction',transactionRouter)
 //? MUST BE BELOW THIS ROUTE
 
