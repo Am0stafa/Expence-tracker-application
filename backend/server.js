881e656f8e0d75591af2452486f97d7664c936d5
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2022-05-22 23:36:53 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use('/api/v1/transaction',transactionRouter)
 //? MUST BE BELOW THIS ROUTE
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname)));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, '..',"build", "index.html"))
